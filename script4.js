5b45b35ca669dc87d49c9e447a972c77589699e2
Rustam-Ulaevich <praporglamur@gmail.com>
2024-11-19 21:31:42 +0500

    5 lesson

--- script4.js
+++ script4.js
@@ -1,4 +1,4 @@
-//Function функции
+// ---- Function declaration функции ----
 
 // function sumNum(a, b){  //a b параметры ф-ции
 //     console.log(a + b);    
@@ -25,13 +25,102 @@
 // showError('Out of memory')
 
 // 3task
-function show5Blocks(n) {
-    console.log(n)
-    for(i=1;i<=n;i++){
-        document.write("<h2> Header "+ i +"</h2>");
-        console.log(n, i)
-    }    
-}
+// function show5Blocks(n) {
+//     console.log(n)
+//     for(i=1;i<=n;i++){
+//         document.write("<h2> Header "+ i +"</h2>");
+//         console.log(n, i)
+//     }    
+// }
     
-show5Blocks(4);
+// show5Blocks(4);
+
+
+// function fakeBin(x){
+//     let array = x.split('') //разбивает строку на массив
+    
+//     for( i=0 ; i<array.length ; i++){
+//         if(array[i]<5){
+//             array[i] = '0'
+//         }else{
+//             array[i] = '1'
+//         }
+//     }
+//     return array.join('')  //разбивает массив на  строку 
+// }
+
+// console.log(fakeBin('162835'))
+
+// Локальные переменные
+
+// function name1(params) {
+//     let message = 'hello'
+//     console.log(message);
+    
+// }
+// // name1()
+// console.log(message);
+
+// Глобальные переменные
+
+// let message = 'hello'
+
+// function name1(params) {
+//     message = 'hi'    
+//     console.log(message);
+// }
+
+// Параметры, аргументы
+
+// function budget(доход, расход){  //a b параметры ф-ции (переменные)
+//     console.log(доход - расход);    
+// }
+
+
+// budget(250000, 150000)    //4 5 аргументы ф-ции
+// console.log(доход - расход)
+
+// Возврат значения  return
+
+// function sumNum(a, b){  
+//   a + b   
+// }    
+// console.log(sumNum(4, 5)) // undefined
+// function sumNum(a, b){  
+//    a + b   
+// }    
+// console.log(sumNum(4, 5))
+
+// ---- Function EXPRESSION функции ----
+
+// func()
+// func1()
+// let func = function(){  //Function EXPRESSION
+//     console.log(123)
+// }
+
+// func1()
+// function func1(){
+//     console.log(456)
+// }
+
+// --- Стрелочные функции ---
+
+// let sum = (a, b) => {
+//     return a+b
+// }
+// console.log(sum(2, 3))
+
+
+// let sum2 = (a, b) => a+b
+
+// console.log(sum2(2, 3))
+
+
+// let sum3 = () => alert(123)
+
+// console.log(sum3())
+
+
+
 
---
+++ script5.js
@@ -0,0 +1,46 @@
+// ООП объектно-ориентированное программирование
+
+// Object
+
+let obj1 = {}          // синтаксис "литерал объекта"
+let obj2 = new Object  // синтаксис "конструктор объекта"
+
+let user = {
+    name: 'Rustam',
+    'age': 15,
+    "date registration": '19.11.2024',
+    teacher: true,
+    pensioner: 45455,
+    address: {
+        city: 'Ufa',
+        street: "Oktyabrya",
+
+    },
+    sayHi: () => console.log('Hello!!!'),    
+}
+console.log(user.sayHi);
+
+// console.log(user.teacher = false) //Изменение зн-я св-ва
+// user.address.city = 'Moscow'      //Изменение зн-я св-ва
+// console.log(user.address.city);
+
+user.newSurname = 'Aetbaev'  //Добавление свойства
+
+ delete user.pensioner    //Удаление свойства
+
+console.log(user.pensioner);
+
+let canvas = {
+    x: 0,
+    y: 2,
+    endX: 10,
+    endY: 15,
+}
+
+for(let tempProperty in canvas){
+    alert(tempProperty)
+    alert(canvas[tempProperty])
+}
+
+
+
