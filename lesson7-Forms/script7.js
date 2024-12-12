c479fc5c58c32014c25d69ea1c898383f3737e33
Rustam-Ulaevich <praporglamur@gmail.com>
2024-12-12 17:59:29 +0500

    7lesson change

--- lesson7-Forms/script7.js
+++ lesson7-Forms/script7.js
@@ -1,9 +1,7 @@
 const formElement = document.querySelector('form')
 
-formElement.addEventListener('submit', (event) => {
-    event.preventDefault()
-
-    const formData = new FormData(formElement)
+formElement.addEventListener('submit', async(event) => {  // в конце добавить 
+    event.preventDefault()    
 
     // const formData = {}
 
@@ -25,8 +23,35 @@ formElement.addEventListener('submit', (event) => {
     //   formData[element.name] = element.value
     // }
 
+    // console.log('formData :', formData);
+
+
+
+    const formData = new FormData(formElement)
+
+    for(data of formData){
+        console.log(data);        
+    }
+
     console.log('formData :', formData);
 
+    // можно найти конкретное значение
+    const content = formData.get('password')
+    console.log(content);
+
+    const formDataAsObject = Object.fromEntries(formData) // 3 для обычного отображения как объект
+    console.log(formDataAsObject);
+    
+
+    formData.set('user', 'New user name')// 2 можно добавить новое значение
+    formData.delete('about')             // 2 удаление значения
+
+
+    await fetch('http://localhost:3000/css', { // 1 при помощи встроенного api fetch отправим тестовый запрос
+        method: 'POST',
+        body: formData
+    })
+    
 
 
 
